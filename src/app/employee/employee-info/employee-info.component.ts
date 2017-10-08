027de7a294452ca41a1a78f6246d43d4d9c8fbad
Unknown <rbocobo@me.com>
2017-10-08 18:21:10 +0800

    Binding

--- src/app/employee/employee-info/employee-info.component.ts
+++ src/app/employee/employee-info/employee-info.component.ts
@@ -6,15 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-info.component.css']
 })
 export class EmployeeInfoComponent implements OnInit {
-
+  employeeId: number;
   firstName: string;
   lastName: string;
   position: string;
+  idpath: string;
+  showIdBorder: boolean;
+  highlightTitle: boolean;
+  isItalic: boolean;
+  isDecorated: boolean;
 
   constructor() {
+    this.employeeId = 1500;
     this.firstName = 'Tyrion';
     this.lastName = 'Lannister';
     this.position = 'Hand of the Queen';
+    this.idpath = '../assets/images/tyrion-id.jpg';
+    this.showIdBorder = true;
+    this.highlightTitle = true;
+    this.isItalic = true;
+    this.isDecorated = true;
   }
 
   getEmployeeName(){
@@ -24,4 +35,19 @@ export class EmployeeInfoComponent implements OnInit {
   ngOnInit() {
   }
 
+  setTitleClass(){
+    let titleClass = {
+      'highlight-title' : this.highlightTitle
+    }
+    return titleClass;
+  }
+
+  setStyle(){
+    let styles = {
+      'font-style' : this.isItalic ? 'italic' : 'normal',
+      'text-decoration' : this.isDecorated ? 'underline' : 'none'
+    }
+
+    return styles;
+  }
 }
