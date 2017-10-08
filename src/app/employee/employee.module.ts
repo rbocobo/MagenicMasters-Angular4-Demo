a5249f26b2e69e6bf15bccf72ac78c4bf40e5b6e
Unknown <rbocobo@me.com>
2017-10-08 19:24:02 +0800

    Two-way binding

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { EmployeeInfoComponent } from './employee-info/employee-info.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports:[EmployeeInfoComponent],
   declarations: [EmployeeInfoComponent]
